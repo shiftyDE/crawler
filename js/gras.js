aa9ddf7c2019f00e0e4355de5fcb4133b464af26
shiftyDE <106715455+shiftyDE@users.noreply.github.com>
2026-08-19 16:10:49 +0200

    fix: typo

--- js/gras.js
+++ js/gras.js
@@ -1,17 +1,17 @@
-        const canvas = document.getElementById('gameCanvas');
-        const ctx = canvas.getContext('2d');
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
 
-        // Gras-Muster zeichnen
-        function drawGrass() {
-            const tileSize = 20;
-            for (let x = 0; x < canvas.width; x += tileSize) {
-                for (let y = 0; y < canvas.height; y += tileSize) {
-                    if ((x / tileSize + y / tileSize) % 2 === 0) {
-                        ctx.fillStyle = '#2d5016';
-                    } else {
-                        ctx.fillStyle = '#3a7a26';
-                    }
-                    ctx.fillRect(x, y, tileSize, tileSize);
-                }
+// Gras-Muster zeichnen
+function drawGrass() {
+    const tileSize = 20;
+    for (let x = 0; x < canvas.width; x += tileSize) {
+        for (let y = 0; y < canvas.height; y += tileSize) {
+            if ((x / tileSize + y / tileSize) % 2 === 0) {
+                ctx.fillStyle = '#2d5016';
+            } else {
+                ctx.fillStyle = '#3a7a26';
             }
+            ctx.fillRect(x, y, tileSize, tileSize);
         }
+    }
+}
