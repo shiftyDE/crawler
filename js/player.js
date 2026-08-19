bc08ddc2ca919a2dc880ad5a1d151039ee8e8f72
shiftyDE <106715455+shiftyDE@users.noreply.github.com>
2026-08-19 16:16:50 +0200

    feat: change character

--- js/player.js
+++ js/player.js
@@ -2,44 +2,130 @@
 function drawPlayer(px, py) {
     const pixelSize = 2;
 
-    // Körper
-    ctx.fillStyle = '#3a7bd5';
-    ctx.fillRect(px + 8 * pixelSize, py + 8 * pixelSize, 16 * pixelSize, 16 * pixelSize);
-    ctx.strokeStyle = '#2a5f9e';
+    // --- Kopf / Helm ---
+    // Helm mit Visier (ähnlich dem typischen Suikoden-Krieger-Helm)
+    ctx.fillStyle = '#c0c0c0';
+    ctx.fillRect(px + 7 * pixelSize, py + 4 * pixelSize, 18 * pixelSize, 9 * pixelSize);
+    ctx.strokeStyle = '#a0a0a0';
     ctx.lineWidth = 1;
-    ctx.strokeRect(px + 8 * pixelSize, py + 8 * pixelSize, 16 * pixelSize, 16 * pixelSize);
+    ctx.strokeRect(px + 7 * pixelSize, py + 4 * pixelSize, 18 * pixelSize, 9 * pixelSize);
 
-    // Kopf
-    ctx.fillStyle = '#f5a623';
-    ctx.fillRect(px + 10 * pixelSize, py + 5 * pixelSize, 8 * pixelSize, 8 * pixelSize);
-    ctx.strokeStyle = '#d4880f';
-    ctx.lineWidth = 1;
-    ctx.strokeRect(px + 10 * pixelSize, py + 5 * pixelSize, 8 * pixelSize, 8 * pixelSize);
+    // Visier
+    ctx.fillStyle = '#333333';
+    ctx.fillRect(px + 9 * pixelSize, py + 5 * pixelSize, 14 * pixelSize, 3 * pixelSize);
+    ctx.fillStyle = '#666666';
+    ctx.fillRect(px + 10 * pixelSize, py + 5 * pixelSize, 12 * pixelSize, 2 * pixelSize);
 
-    // Augen
-    ctx.fillStyle = '#1a1a1a';
-    ctx.fillRect(px + 11 * pixelSize, py + 6 * pixelSize, 2 * pixelSize, 2 * pixelSize);
-    ctx.fillRect(px + 15 * pixelSize, py + 6 * pixelSize, 2 * pixelSize, 2 * pixelSize);
-
-    // Mund
+    // --- Rüstung / Torso ---
+    // Rüstung mit Schulterpanzer
     ctx.fillStyle = '#8b0000';
-    ctx.fillRect(px + 12 * pixelSize, py + 12 * pixelSize, 2 * pixelSize, 1 * pixelSize);
-
-    // Arme
-    ctx.fillStyle = '#3a7bd5';
-    ctx.fillRect(px + 5 * pixelSize, py + 10 * pixelSize, 3 * pixelSize, 2 * pixelSize);
-    ctx.fillRect(px + 21 * pixelSize, py + 10 * pixelSize, 3 * pixelSize, 2 * pixelSize);
-    ctx.strokeStyle = '#2a5f9e';
+    ctx.fillRect(px + 6 * pixelSize, py + 13 * pixelSize, 20 * pixelSize, 12 * pixelSize);
+    ctx.strokeStyle = '#6b0000';
     ctx.lineWidth = 1;
-    ctx.strokeRect(px + 5 * pixelSize, py + 10 * pixelSize, 3 * pixelSize, 2 * pixelSize);
-    ctx.strokeRect(px + 21 * pixelSize, py + 10 * pixelSize, 3 * pixelSize, 2 * pixelSize);
+    ctx.strokeRect(px + 6 * pixelSize, py + 13 * pixelSize, 20 * pixelSize, 12 * pixelSize);
 
-    // Beine
-    ctx.fillStyle = '#3a7bd5';
-    ctx.fillRect(px + 9 * pixelSize, py + 20 * pixelSize, 3 * pixelSize, 4 * pixelSize);
-    ctx.fillRect(px + 19 * pixelSize, py + 20 * pixelSize, 3 * pixelSize, 4 * pixelSize);
-    ctx.strokeStyle = '#2a5f9e';
+    // Schulterpanzer links
+    ctx.fillStyle = '#8b0000';
+    ctx.fillRect(px + 3 * pixelSize, py + 13 * pixelSize, 3 * pixelSize, 4 * pixelSize);
+    ctx.fillRect(px + 3 * pixelSize, py + 13 * pixelSize, 3 * pixelSize, 3 * pixelSize);
+    ctx.fillStyle = '#6b0000';
+    ctx.fillRect(px + 3 * pixelSize, py + 13 * pixelSize, 3 * pixelSize, 3 * pixelSize);
+
+    // Schulterpanzer rechts
+    ctx.fillStyle = '#8b0000';
+    ctx.fillRect(px + 23 * pixelSize, py + 13 * pixelSize, 3 * pixelSize, 4 * pixelSize);
+    ctx.fillRect(px + 23 * pixelSize, py + 13 * pixelSize, 3 * pixelSize, 3 * pixelSize);
+    ctx.fillStyle = '#6b0000';
+    ctx.fillRect(px + 23 * pixelSize, py + 13 * pixelSize, 3 * pixelSize, 3 * pixelSize);
+
+    // --- Arme ---
+    // Linker Arm
+    ctx.fillStyle = '#8b0000';
+    ctx.fillRect(px + 2 * pixelSize, py + 15 * pixelSize, 4 * pixelSize, 6 * pixelSize);
+    ctx.strokeStyle = '#6b0000';
     ctx.lineWidth = 1;
-    ctx.strokeRect(px + 9 * pixelSize, py + 20 * pixelSize, 3 * pixelSize, 4 * pixelSize);
-    ctx.strokeRect(px + 19 * pixelSize, py + 20 * pixelSize, 3 * pixelSize, 4 * pixelSize);
+    ctx.strokeRect(px + 2 * pixelSize, py + 15 * pixelSize, 4 * pixelSize, 6 * pixelSize);
+
+    // Linker Arm - Handschuh
+    ctx.fillStyle = '#c0c0c0';
+    ctx.fillRect(px + 1 * pixelSize, py + 14 * pixelSize, 4 * pixelSize, 2 * pixelSize);
+    ctx.strokeStyle = '#a0a0a0';
+    ctx.lineWidth = 1;
+    ctx.strokeRect(px + 1 * pixelSize, py + 14 * pixelSize, 4 * pixelSize, 2 * pixelSize);
+
+    // Rechter Arm
+    ctx.fillStyle = '#8b0000';
+    ctx.fillRect(px + 22 * pixelSize, py + 15 * pixelSize, 4 * pixelSize, 6 * pixelSize);
+    ctx.strokeStyle = '#6b0000';
+    ctx.lineWidth = 1;
+    ctx.strokeRect(px + 22 * pixelSize, py + 15 * pixelSize, 4 * pixelSize, 6 * pixelSize);
+
+    // Rechter Arm - Handschuh
+    ctx.fillStyle = '#c0c0c0';
+    ctx.fillRect(px + 23 * pixelSize, py + 14 * pixelSize, 4 * pixelSize, 2 * pixelSize);
+    ctx.strokeStyle = '#a0a0a0';
+    ctx.lineWidth = 1;
+    ctx.strokeRect(px + 23 * pixelSize, py + 14 * pixelSize, 4 * pixelSize, 2 * pixelSize);
+
+    // --- Beine ---
+    // linke Hose / Bein
+    ctx.fillStyle = '#1a1a1a';
+    ctx.fillRect(px + 8 * pixelSize, py + 25 * pixelSize, 4 * pixelSize, 6 * pixelSize);
+    ctx.strokeStyle = '#0a0a0a';
+    ctx.lineWidth = 1;
+    ctx.strokeRect(px + 8 * pixelSize, py + 25 * pixelSize, 4 * pixelSize, 6 * pixelSize);
+
+    // rechte Hose / Bein
+    ctx.fillStyle = '#1a1a1a';
+    ctx.fillRect(px + 18 * pixelSize, py + 25 * pixelSize, 4 * pixelSize, 6 * pixelSize);
+    ctx.strokeStyle = '#0a0a0a';
+    ctx.lineWidth = 1;
+    ctx.strokeRect(px + 18 * pixelSize, py + 25 * pixelSize, 4 * pixelSize, 6 * pixelSize);
+
+    // --- Stiefel ---
+    // linker Stiefel
+    ctx.fillStyle = '#222222';
+    ctx.fillRect(px + 7 * pixelSize, py + 31 * pixelSize, 4 * pixelSize, 3 * pixelSize);
+    ctx.strokeStyle = '#111111';
+    ctx.lineWidth = 1;
+    ctx.strokeRect(px + 7 * pixelSize, py + 31 * pixelSize, 4 * pixelSize, 3 * pixelSize);
+
+    // rechter Stiefel
+    ctx.fillStyle = '#222222';
+    ctx.fillRect(px + 19 * pixelSize, py + 31 * pixelSize, 4 * pixelSize, 3 * pixelSize);
+    ctx.strokeStyle = '#111111';
+    ctx.lineWidth = 1;
+    ctx.strokeRect(px + 19 * pixelSize, py + 31 * pixelSize, 4 * pixelSize, 3 * pixelSize);
+
+    // --- Schwert (in der rechten Hand) ---
+    // Klinge
+    ctx.fillStyle = '#cccccc';
+    ctx.fillRect(px + 22 * pixelSize, py + 13 * pixelSize, 2 * pixelSize, 10 * pixelSize);
+    ctx.strokeStyle = '#aaaaaa';
+    ctx.lineWidth = 1;
+    ctx.strokeRect(px + 22 * pixelSize, py + 13 * pixelSize, 2 * pixelSize, 10 * pixelSize);
+
+    // Klinge - Kante
+    ctx.fillStyle = '#eeeeee';
+    ctx.fillRect(px + 23 * pixelSize, py + 13 * pixelSize, 1 * pixelSize, 10 * pixelSize);
+    ctx.strokeStyle = '#dddddd';
+    ctx.lineWidth = 1;
+    ctx.strokeRect(px + 23 * pixelSize, py + 13 * pixelSize, 1 * pixelSize, 10 * pixelSize);
+
+    // Klinge - Griffe
+    ctx.fillStyle = '#8b4513';
+    ctx.fillRect(px + 20 * pixelSize, py + 13 * pixelSize, 6 * pixelSize, 2 * pixelSize);
+    ctx.fillStyle = '#6b3513';
+    ctx.fillRect(px + 20 * pixelSize, py + 13 * pixelSize, 6 * pixelSize, 2 * pixelSize);
+
+    // --- Details: Helm-Plume / Feder ---
+    // Feder auf dem Helm
+    ctx.fillStyle = '#ff0000';
+    ctx.fillRect(px + 14 * pixelSize, py + 3 * pixelSize, 2 * pixelSize, 3 * pixelSize);
+    ctx.fillStyle = '#cc0000';
+    ctx.fillRect(px + 14 * pixelSize, py + 3 * pixelSize, 2 * pixelSize, 2 * pixelSize);
+
+    // --- Details: Visier-Reflex ---
+    ctx.fillStyle = '#555555';
+    ctx.fillRect(px + 10 * pixelSize, py + 6 * pixelSize, 12 * pixelSize, 1 * pixelSize);
 }
