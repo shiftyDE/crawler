d3305e697248a71de2e77124eb26f9f57a8897c5
shiftyDE <106715455+shiftyDE@users.noreply.github.com>
2026-08-19 17:23:24 +0200

    feat: Place a tree

--- js/player.js
+++ js/player.js
@@ -33,6 +33,9 @@ function gameLoop() {
     }
     drawGrass();
 
+    // Baum in der Mitte zeichnen
+    drawTree();
+
     // Spieler zeichnen
     drawPlayer(playerX, playerY);
 
@@ -172,4 +175,38 @@ function drawPlayer(px, py) {
     // --- Details: Visier-Reflex ---
     ctx.fillStyle = '#555555';
     ctx.fillRect(px + 10 * pixelSize, py + 6 * pixelSize, 12 * pixelSize, 1 * pixelSize);
+}
+
+// Baum zeichnen (in der Mitte der Map)
+function drawTree() {
+    const centerX = canvas.width / 2;
+    const centerY = canvas.height / 2;
+
+    // Baumstamm
+    ctx.fillStyle = '#8B4513';
+    ctx.fillRect(centerX - 5, centerY - 40, 10, 40);
+
+    // Baumkronen (mehrere Kreise für einen natürlichen Look)
+    ctx.fillStyle = '#228B22';
+    ctx.beginPath();
+    ctx.arc(centerX - 8, centerY - 55, 18, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.beginPath();
+    ctx.arc(centerX + 8, centerY - 55, 18, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.beginPath();
+    ctx.arc(centerX, centerY - 70, 20, 0, Math.PI * 2);
+    ctx.fill();
+
+    // Dunklere Blätter für Tiefe
+    ctx.fillStyle = '#1a6b1a';
+    ctx.beginPath();
+    ctx.arc(centerX - 10, centerY - 52, 15, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.beginPath();
+    ctx.arc(centerX + 10, centerY - 52, 15, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.beginPath();
+    ctx.arc(centerX, centerY - 67, 17, 0, Math.PI * 2);
+    ctx.fill();
 }
