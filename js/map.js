f84d4f514612ab1eb79ff26126f9d0393758f961
shiftyDE <106715455+shiftyDE@users.noreply.github.com>
2026-08-19 17:52:57 +0200

    feat: Move the code for Grass to the map.js file

--- js/map.js
+++ js/map.js
@@ -1,3 +1,18 @@
+// Gras zeichnen
+function drawGrass() {
+    const tileSize = 20;
+    for (let x = 0; x < canvas.width; x += tileSize) {
+        for (let y = 0; y < canvas.height; y += tileSize) {
+            if ((x / tileSize + y / tileSize) % 2 === 0) {
+                ctx.fillStyle = '#2d5016';
+            } else {
+                ctx.fillStyle = '#3a7a26';
+            }
+            ctx.fillRect(x, y, tileSize, tileSize);
+        }
+    }
+}
+
 // Baum zeichnen (in der Mitte der Map)
 function drawTree() {
     const centerX = canvas.width / 2;
--- js/player.js
+++ js/player.js
@@ -18,19 +18,6 @@ function gameLoop() {
     playerY = Math.max(0, Math.min(playerY, canvas.height - playerSize));
 
     // Gras zeichnen
-    function drawGrass() {
-        const tileSize = 20;
-        for (let x = 0; x < canvas.width; x += tileSize) {
-            for (let y = 0; y < canvas.height; y += tileSize) {
-                if ((x / tileSize + y / tileSize) % 2 === 0) {
-                    ctx.fillStyle = '#2d5016';
-                } else {
-                    ctx.fillStyle = '#3a7a26';
-                }
-                ctx.fillRect(x, y, tileSize, tileSize);
-            }
-        }
-    }
     drawGrass();
 
     // Baum in der Mitte zeichnen
